7776df79f55f7453eb7fa9b711d21e618ff22802
Lyudmil Goranov <102062855+Goran0v@users.noreply.github.com>
2024-08-16 14:14:30 +0300

    Added 3 more exercises

---
+++ FirstStepsInCodingExercise/SuppliesForSchool/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace SuppliesForSchool
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int packetsPens = int.Parse(Console.ReadLine());
+            int packetsMarkers = int.Parse(Console.ReadLine());
+            int littersDetergent = int.Parse(Console.ReadLine());
+            double procent = double.Parse(Console.ReadLine());
+            double discount = procent / 100;
+
+            double money = (packetsPens * 5.8 + packetsMarkers * 7.2 + littersDetergent * 1.2);
+            double moneyWithDiscount = money - money * discount;
+            Console.WriteLine(moneyWithDiscount);
+        }
+    }
+}
