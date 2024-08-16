47616913e4cff704f1f4778796843c8dda544fa1
Lyudmil Goranov <102062855+Goran0v@users.noreply.github.com>
2024-08-16 14:56:10 +0300

    Add 3 more exercises

---
+++ AdvancedConditionalStatements/WorkingHours/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace WorkingHours
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int hour = int.Parse(Console.ReadLine());
+            string day = Console.ReadLine();
+
+            if (hour >= 10 && hour <= 18)
+            {
+                if (day == "Monday")
+                {
+                    Console.WriteLine("open");
+                }
+                if (day == "Tuesday")
+                {
+                    Console.WriteLine("open");
+                }
+                if (day == "Wednesday")
+                {
+                    Console.WriteLine("open");
+                }
+                if (day == "Thursday")
+                {
+                    Console.WriteLine("open");
+                }
+                if (day == "Friday")
+                {
+                    Console.WriteLine("open");
+                }
+                if (day == "Saturday")
+                {
+                    Console.WriteLine("open");
+                }
+                if (day == "Sunday")
+                {
+                    Console.WriteLine("closed");
+                }
+            }
+            else
+            {
+                Console.WriteLine("closed");
+            }
+        }
+    }
+}
