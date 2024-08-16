564e4880216088f592ea7875176b370590a5b7a6
Lyudmil Goranov <102062855+Goran0v@users.noreply.github.com>
2024-08-16 14:32:02 +0300

    Add first 3 exercises

---
+++ ConditionalStatements/AreaOfFigures/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace AreaOfFigures
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string figure = Console.ReadLine();
+            if (figure == "square")
+            {
+                double a = double.Parse(Console.ReadLine());
+                double area = a * a;
+                Console.WriteLine("{0:F3}", area);
+            }
+            if (figure == "rectangle")
+            {
+                double a = double.Parse(Console.ReadLine());
+                double b = double.Parse(Console.ReadLine());
+                double area = a * b;
+                Console.WriteLine("{0:F3}", area);
+            }
+            if (figure == "circle")
+            {
+                double r = double.Parse(Console.ReadLine());
+                double area = r * r * Math.PI;
+                Console.WriteLine("{0:F3}", area);
+            }
+            if (figure == "triangle")
+            {
+                double b = double.Parse(Console.ReadLine());
+                double h = double.Parse(Console.ReadLine());
+                double area = (b * h) / 2;
+                Console.WriteLine("{0:F3}", area);
+            }
+        }
+    }
+}
---
+++ ConditionalStatements/GreaterNumber/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GreaterNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
