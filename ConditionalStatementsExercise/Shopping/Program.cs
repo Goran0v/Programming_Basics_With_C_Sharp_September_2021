f6eedf0cfb5ba8643a15a3096c6f92d9e6038006
Lyudmil Goranov <102062855+Goran0v@users.noreply.github.com>
2024-08-16 14:48:46 +0300

    Add 3 exercises

---
+++ ConditionalStatementsExercise/Shopping/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Shopping
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const double videocardPrice = 250;
+
+            double budget = double.Parse(Console.ReadLine());
+            int numberOfVideoCards = int.Parse(Console.ReadLine());
+            int numberOfProcessors = int.Parse(Console.ReadLine());
+            int numberOfRamMemory = int.Parse(Console.ReadLine());
+
+            double totalVideoCardPrice = numberOfVideoCards * videocardPrice;
+            double totalProcessorsPrice = totalVideoCardPrice * 0.35;
+            double totalRamMemoryPrice = totalVideoCardPrice * 0.1;
+
+            double totalMoney = totalVideoCardPrice + totalProcessorsPrice * numberOfProcessors + totalRamMemoryPrice * numberOfRamMemory;
+
+            if (numberOfVideoCards > numberOfProcessors)
+            {
+                totalMoney *= 0.85;
+            }
+
+
+            double difference = Math.Abs(totalMoney - budget);
+
+            
+            
+            Console.WriteLine(budget >= totalMoney
+            ? $"You have {difference:f2} leva left!"
+            : $"Not enough money! You need {difference:f2} leva more!");
+            
+        }
+    }
+}
