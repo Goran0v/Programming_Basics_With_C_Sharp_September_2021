7d65279d2c8c63ced31b37f933ad66e10a3987a6
Lyudmil Goranov <102062855+Goran0v@users.noreply.github.com>
2024-08-16 14:57:26 +0300

    Add last 3 exercises

---
+++ AdvancedConditionalStatements/TradeCommissions/Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+
+namespace TradeCommissions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string city = Console.ReadLine();
+            double trades = double.Parse(Console.ReadLine());
+            double percent = 0;
+
+            if (city == "Sofia")
+            {
+                if (trades >= 0 && trades <= 500)
+                {
+                    percent = 0.05;
+                }
+                else if (trades > 500 && trades <= 1000)
+                {
+                    percent = 0.07;
+                }
+                else if (trades > 1000 && trades <= 10000)
+                {
+                    percent = 0.08;
+                }
+                else if (trades > 10000)
+                {
+                    percent = 0.12;
+                }
+                else
+                {
+                    Console.WriteLine("error");
+                }
+            }
+
+            else if (city == "Varna")
+            {
+                if (trades >= 0 && trades <= 500)
+                {
+                    percent = 0.045;
+                }
+                else if (trades > 500 && trades <= 1000)
+                {
+                    percent = 0.075;
+                }
+                else if (trades > 1000 && trades <= 10000)
+                {
+                    percent = 0.1;
+                }
+                else if (trades > 10000)
+                {
+                    percent = 0.13;
+                }
+                else
+                {
+                    Console.WriteLine("error");
+                }
+            }
+
+            else if (city == "Plovdiv")
+            {
+                if (trades >= 0 && trades <= 500)
+                {
+                    percent = 0.055;
+                }
+                else if (trades > 500 && trades <= 1000)
+                {
+                    percent = 0.08;
+                }
+                else if (trades > 1000 && trades <= 10000)
+                {
+                    percent = 0.12;
+                }
+                else if (trades > 10000)
+                {
+                    percent = 0.145;
+                }
+                else
+                {
+                    Console.WriteLine("error");
+                }
+            }
+
+            else
+            {
+                Console.WriteLine("error");
+            }
+
+            double commission = trades * percent;
+
+            if (commission != 0)
+            {
+                Console.WriteLine($"{commission:f2}");
+            }
+            
+        }
+    }
+}
