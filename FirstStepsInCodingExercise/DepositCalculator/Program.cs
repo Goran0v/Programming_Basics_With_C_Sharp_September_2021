fe2ccf4b01de99ae6e442b5b6bf9101440588c72
Lyudmil Goranov <102062855+Goran0v@users.noreply.github.com>
2024-08-16 14:11:43 +0300

    Added first 3 exercises

---
+++ FirstStepsInCodingExercise/DepositCalculator/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace DepositCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double depositSum = double.Parse(Console.ReadLine());
+            int term = int.Parse(Console.ReadLine());
+            double procent = double.Parse(Console.ReadLine());
+            double interest = depositSum * procent / 100;
+            double interestForOneMonth = interest / 12;
+            double sum = depositSum + term * interestForOneMonth;
+
+            Console.WriteLine(sum);
+        }
+    }
+}
