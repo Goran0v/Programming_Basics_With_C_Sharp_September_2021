6ea2bc15c0321190f8e23fc31380c8f77f0eca7b
Lyudmil Goranov <102062855+Goran0v@users.noreply.github.com>
2024-08-17 15:49:23 +0300

    Add 3 more exercises

---
+++ ForLoopExercise/Oscars/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Oscars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const double NEEDED_POINTS_FOR_OSKAR = 1250.5;
+            string nameOfActor = Console.ReadLine();
+            double pointsFromAcademy = double.Parse(Console.ReadLine());
+            int jury = int.Parse(Console.ReadLine());
+
+            double sumPoints = pointsFromAcademy;
+
+            bool isActorFound = false;
+            for (int i = 0; i < jury; i++)
+            {
+                string nameOfJury = Console.ReadLine();
+                double givenPoints = double.Parse(Console.ReadLine());
+
+                int lengthOfName = nameOfJury.Length;
+                double momentPoints = (lengthOfName * givenPoints) / 2;
+                sumPoints += momentPoints;
+
+                if (sumPoints >= NEEDED_POINTS_FOR_OSKAR)
+                {
+                    isActorFound = true;
+                    break;
+                }
+            }
+            if (isActorFound)
+            {
+                Console.WriteLine($"Congratulations, {nameOfActor} got a nominee for leading role with {sumPoints:f1}!");
+            }
+            else
+            {
+                double neededPoints = NEEDED_POINTS_FOR_OSKAR - sumPoints;
+                Console.WriteLine($"Sorry, {nameOfActor} you need {neededPoints:f1} more!");
+            }
+        }
+    }
+}
---
+++ ForLoopExercise/Salary/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Salary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int FACEBOOK = 150;
+            const int INSTAGRAM = 100;
+            const int REDDIT = 50;
+
+            int openTabs = int.Parse(Console.ReadLine());
+            int salary = int.Parse(Console.ReadLine());
+            for (int i = 0; i <= openTabs; i++)
+            {
+                if (salary <= 0)
+                {
+                    Console.WriteLine($"You have lost your salary.");
+                    break;
+                }
+                string openedTab = Console.ReadLine();
+                if (openedTab == "Facebook")
+                {
+                    salary -= FACEBOOK;
+                }
+                else if (openedTab == "Instagram")
+                {
+                    salary -= INSTAGRAM;
+                }
+                else if (openedTab == "Reddit")
+                {
+                    salary -= REDDIT;
+                }
+            }
+
+            if (salary > 0)
+            {
+                Console.WriteLine(salary);
+            }
+        }
+    }
+}
