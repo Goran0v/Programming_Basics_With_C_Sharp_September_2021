90fd16c033505c6ebad0ba83b214308a15d1d9ae
Lyudmil Goranov <102062855+Goran0v@users.noreply.github.com>
2024-08-17 15:55:03 +0300

    Add 3 more exercises

---
+++ WhileLoop/MinNumber/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace MaxNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = (Console.ReadLine());
+            int minNum = int.MaxValue;
+
+            while (input != "Stop")
+            {
+                int currentNum = int.Parse(input);
+                if (minNum > currentNum)
+                {
+                    minNum = currentNum;
+                }
+                input = Console.ReadLine();
+            }
+            Console.WriteLine(minNum);
+        }
+    }
+}
