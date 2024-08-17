fd953a1715a966f7c8a880345ae8481252a83a21
Lyudmil Goranov <102062855+Goran0v@users.noreply.github.com>
2024-08-17 16:01:56 +0300

    Add last 3 exercises

---
+++ WhileLoopExercise/Moving/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Moving
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int widthOfFreeSpace = int.Parse(Console.ReadLine());
+            int lengthOfFreeSpace = int.Parse(Console.ReadLine());
+            int heightOfFreeSpace = int.Parse(Console.ReadLine());
+            int freeSpace = widthOfFreeSpace * lengthOfFreeSpace * heightOfFreeSpace;
+            int sumOfCubicMeters = 0;
+            string input = Console.ReadLine();
+            while (input != "Done")
+            {
+                int currentCubicMeters = int.Parse(input);
+                sumOfCubicMeters += currentCubicMeters;
+                if (freeSpace < sumOfCubicMeters)
+                {
+                    int neededCubicMeters = sumOfCubicMeters - freeSpace;
+                    Console.WriteLine($"No more free space! You need {neededCubicMeters} Cubic meters more.");
+                    break;
+                }
+                input = Console.ReadLine();
+            }
+            if (input == "Done")
+            {
+                int freeCubicMeters = freeSpace - sumOfCubicMeters;
+                Console.WriteLine($"{freeCubicMeters} Cubic meters left.");
+            }
+        }
+    }
+}
