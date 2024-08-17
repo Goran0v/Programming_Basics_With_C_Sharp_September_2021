3001091847010cb5ef667c9ed75a563ee6ee045d
Lyudmil Goranov <102062855+Goran0v@users.noreply.github.com>
2024-08-17 15:40:44 +0300

    Add 3 more exercises

---
+++ AdvancedConditionalStatementsExercise/OnTimeForTheExam/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace OnTimeForTheExam
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int hourOfTheExam = int.Parse(Console.ReadLine());
+            int minuteOfTheExam = int.Parse(Console.ReadLine());
+            int hourOfArrival = int.Parse(Console.ReadLine());
+            int minuteOfArrival = int.Parse(Console.ReadLine());
+
+            int difference = 0;
+            int hour = 0;
+            int minutes = 0;
+
+            minuteOfTheExam += hourOfTheExam * 60;
+            minuteOfArrival += hourOfArrival * 60;
+
+            if (minuteOfArrival > minuteOfTheExam)
+            {
+                Console.WriteLine("Late");
+                difference = minuteOfArrival - minuteOfTheExam;
+                if (difference < 60)
+                {
+                    Console.WriteLine($"{difference} minutes after the start");
+                }
+                else
+                {
+                    hour = difference / 60;
+                    minutes = difference % 60;
+                    Console.WriteLine($"{hour}:{minutes:d2} hours after the start");
+                }
+            }
+            else if (minuteOfArrival < minuteOfTheExam - 30)
+            {
+                Console.WriteLine("Early");
+                difference = minuteOfTheExam - minuteOfArrival;
+                if (difference < 60)
+                {
+                    Console.WriteLine($"{difference} minutes before the start");
+                }
+                else
+                {
+                    hour = difference / 60;
+                    minutes = difference % 60;
+                    Console.WriteLine($"{hour}:{minutes:d2} hours before the start");
+                }
+            }
+            else
+            {
+                Console.WriteLine("On time");
+                difference = minuteOfTheExam - minuteOfArrival;
+                Console.WriteLine($"{difference} minutes before the start");
+            }
+        }
+    }
+}
---
+++ AdvancedConditionalStatementsExercise/OperationsBetweenNumbers/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+
+namespace OperationsBetweenNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N1 = int.Parse(Console.ReadLine());
+            int N2 = int.Parse(Console.ReadLine());
+            string op = Console.ReadLine();
+            double result = 0;
+
+            if (op == "+")
+            {
+                result = N1 + N2;
+                if (result % 2 == 0)
+                {
+                    Console.WriteLine($"{N1} {op} {N2} = {result} - even");
+                }
+                else if (result % 2 != 0)
+                {
+                    Console.WriteLine($"{N1} {op} {N2} = {result} - odd");
+                }
+            }
+
+            if (op == "-")
+            {
+                result = (double)N1 - (double)N2;
+                if (result % 2 == 0)
+                {
+                    Console.WriteLine($"{N1} {op} {N2} = {result} - even");
+                }
+                else if (result % 2 != 0)
+                {
+                    Console.WriteLine($"{N1} {op} {N2} = {result} - odd");
+                }
+            }
+
+            if (op == "*")
+            {
+                result = (double)N1 * (double)N2;
+                if (result % 2 == 0)
+                {
+                    Console.WriteLine($"{N1} {op} {N2} = {result} - even");
+                }
+                else if (result % 2 != 0)
+                {
+                    Console.WriteLine($"{N1} {op} {N2} = {result} - odd");
+                }
+            }
+
+            if (op == "/" && N2 != 0)
+            {
+                result = (double)N1 / (double)N2;
+                Console.WriteLine($"{N1} {op} {N2} = {result:f2}");
+            }
+            else if (op == "/" && N2 == 0)
+            {
+                Console.WriteLine($"Cannot divide {N1} by zero");
+            }
+
+            if (op == "%" && N2 != 0)
+            {
+                result = N1 % N2;
+                Console.WriteLine($"{N1} {op} {N2} = {result}");
+            }
+            else if (op == "%" && N2 == 0)
+            {
+                Console.WriteLine($"Cannot divide {N1} by zero");
+            }
+        }
+    }
+}
