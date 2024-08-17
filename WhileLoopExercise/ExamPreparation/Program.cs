660e7594b9136494fb3f44eadcaed9be83b220b5
Lyudmil Goranov <102062855+Goran0v@users.noreply.github.com>
2024-08-17 16:01:04 +0300

    Add first 3 exercises

---
+++ WhileLoopExercise/ExamPreparation/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace ExamPreparation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int BAD_GRADE = 4;
+            int badMarks = int.Parse(Console.ReadLine());
+            int badGradesCount = 0;
+            string currentProblem = Console.ReadLine();
+            string lastProblem = "";
+            int gradesSum = 0;
+            int allGrades = 0;
+
+            while (currentProblem != "Enough")
+            {
+                int currentGrade = int.Parse(Console.ReadLine());
+                gradesSum++;
+                allGrades += currentGrade;
+                if (currentGrade <= BAD_GRADE)
+                {
+                    badGradesCount++;
+                    if (badGradesCount == badMarks)
+                    {
+                        break;
+                    }
+                }
+                lastProblem = currentProblem;
+                currentProblem = Console.ReadLine();
+            }
+            if (currentProblem == "Enough")
+            {
+                double averageScore = 1.0 * allGrades / gradesSum;
+                Console.WriteLine($"Average score: {averageScore:f2}");
+                Console.WriteLine($"Number of problems: {gradesSum}");
+                Console.WriteLine($"Last problem: {lastProblem}");
+            }
+            else
+            {
+                Console.WriteLine($"You need a break, {badGradesCount} poor grades.");
+            }
+        }
+    }
+}
---
+++ WhileLoopExercise/Walking/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Walking
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int GOAL = 10000;
+            int allSteps = 0;
+            
+
+            while (allSteps < GOAL)
+            {
+                string command = Console.ReadLine();
+                if (command == "Going home")
+                {
+                    int stepsLeft = int.Parse(Console.ReadLine());
+                    allSteps += stepsLeft;
+                    break;
+                }
+
+                int currentSteps = int.Parse(command);
+                allSteps += currentSteps;
+
+            }
+            if (allSteps >= GOAL)
+            {
+                Console.WriteLine("Goal reached! Good job!");
+                int stepsOverTheGoal = allSteps - GOAL;
+                Console.WriteLine($"{stepsOverTheGoal} steps over the goal!");
+            }
+            else
+            {
+                int diff = GOAL - allSteps;
+                Console.WriteLine($"{diff} more steps to reach goal.");
+            }
+        }
+    }
+}
