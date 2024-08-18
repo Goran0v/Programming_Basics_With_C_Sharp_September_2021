77f3006c4ff8f46db28181f04fb7484397cba473
Lyudmil Goranov <102062855+Goran0v@users.noreply.github.com>
2024-08-18 14:20:16 +0300

    Add first 3 exercises

---
+++ FirstExamPrep/ExamPreparation/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ExamPreparation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nameOfFilm = Console.ReadLine();
+            int days = int.Parse(Console.ReadLine());
+            int tickets = int.Parse(Console.ReadLine());
+            double priceOfTicket = double.Parse(Console.ReadLine());
+            int procentsForTheCinema = int.Parse(Console.ReadLine());
+
+            double sumOfTickets = tickets * priceOfTicket;
+            double allSum = sumOfTickets * days;
+            double finalSum = ((100 - (double)procentsForTheCinema) / 100) * allSum;
+            Console.WriteLine($"The profit from the movie {nameOfFilm} is {finalSum:f2} lv.");
+        }
+    }
+}
---
+++ FirstExamPrep/FilmRating/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace FilmRating
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfFilms = int.Parse(Console.ReadLine());
+            double highestRating = double.MinValue;
+            double lowestRating = double.MaxValue;
+            string nameOfFilmWithHighestRating = "";
+            string nameOfFilmWithLowestRating = "";
+            double averageRating = 0;
+            int counter = 0;
+            double allRating = 0;
+
+            for (int i = 1; i <= numberOfFilms; i++)
+            {
+                string nameOfFilm = Console.ReadLine();
+                double rating = double.Parse(Console.ReadLine());
+                counter++;
+                allRating += rating;
+
+                if (highestRating < rating)
+                {
+                    highestRating = rating;
+                    nameOfFilmWithHighestRating = nameOfFilm;
+                }
+                if (lowestRating > rating)
+                {
+                    lowestRating = rating;
+                    nameOfFilmWithLowestRating = nameOfFilm;
+                }
+            }
+            averageRating = allRating / counter;
+            Console.WriteLine($"{nameOfFilmWithHighestRating} is with highest rating: {highestRating:f1}");
+            Console.WriteLine($"{nameOfFilmWithLowestRating} is with lowest rating: {lowestRating:f1}");
+            Console.WriteLine($"Average rating: {averageRating:f1}");
+        }
+    }
+}
