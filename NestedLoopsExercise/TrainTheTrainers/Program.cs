3d778759623df65b46002008e3a8b9e1b0aca4ec
Lyudmil Goranov <102062855+Goran0v@users.noreply.github.com>
2024-08-18 14:09:26 +0300

    Add last 3 exercises

---
+++ NestedLoopsExercise/TrainTheTrainers/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace TrainTheTrainers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int jury = int.Parse(Console.ReadLine());
+            string nameOfPresentation = Console.ReadLine();
+            int gradeCount = 0;
+            double sum = 0;
+            while (nameOfPresentation != "Finish")
+            {
+                double sumOfGrades = 0;
+                for (int i = 1; i <= jury; i++)
+                {
+                    double marks = double.Parse(Console.ReadLine());
+                    sumOfGrades += marks;
+                    gradeCount++;
+                    sum += marks;
+                }
+                double average = sumOfGrades / jury;
+                Console.WriteLine($"{nameOfPresentation} - {average:f2}.");
+                nameOfPresentation = Console.ReadLine();
+            }
+            double finalAverage = sum / gradeCount;
+            Console.WriteLine($"Student's final assessment is {finalAverage:f2}.");
+        }
+    }
+}
