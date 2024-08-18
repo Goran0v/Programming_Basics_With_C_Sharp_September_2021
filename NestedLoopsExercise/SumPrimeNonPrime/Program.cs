874b8ed9c878218242cceec97a28757cc3882fa5
Lyudmil Goranov <102062855+Goran0v@users.noreply.github.com>
2024-08-18 14:08:20 +0300

    Add first 3 exercises

---
+++ NestedLoopsExercise/SumPrimeNonPrime/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace SumPrimeNonPrime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            int sumOfPrime = 0;
+            int sumOfNonPrime = 0;
+            while (input != "stop")
+            {
+                int num = int.Parse(input);
+                if (num < 0)
+                {
+                    Console.WriteLine("Number is negative.");
+                }
+                else
+                {
+                    int count = 0;
+                    for (int i = 1; i <= num; i++)
+                    {
+                        if (num % i == 0)
+                        {
+                            count++;
+                        }
+                    }
+                    if (count == 2)
+                    {
+                        sumOfPrime += num;
+                    }
+                    else
+                    {
+                        sumOfNonPrime += num;
+                    }
+                }
+                input = Console.ReadLine();
+            }
+            Console.WriteLine($"Sum of all prime numbers is: {sumOfPrime}");
+            Console.WriteLine($"Sum of all non prime numbers is: {sumOfNonPrime}");
+        }
+    }
+}
