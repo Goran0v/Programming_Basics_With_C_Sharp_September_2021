1f48107aaede8092de172483d4b0718833771613
Lyudmil Goranov <102062855+Goran0v@users.noreply.github.com>
2024-08-18 14:35:40 +0300

    Add last 3 exercises

---
+++ Exam/HairSalon/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace HairSalon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int goalOfTheDay = int.Parse(Console.ReadLine());
+            string input = Console.ReadLine();
+            string inputForSalon = "";
+            double sum = 0;
+            double allPrice = 0;
+
+            while (input != "closed")
+            {
+                if (input == "haircut")
+                {
+                    inputForSalon = Console.ReadLine();
+                    if (inputForSalon == "mens")
+                    {
+                        sum = 15;
+                    }
+                    else if (inputForSalon == "ladies")
+                    {
+                        sum = 20;
+                    }
+                    else if (inputForSalon == "kids")
+                    {
+                        sum = 10;
+                    }
+                }
+                else if (input == "color")
+                {
+                    inputForSalon = Console.ReadLine();
+                    if (inputForSalon == "touch up")
+                    {
+                        sum = 20;
+                    }
+                    else if (inputForSalon == "full color")
+                    {
+                        sum = 30;
+                    }
+                }
+                allPrice += sum;
+                if (allPrice >= goalOfTheDay)
+                {
+                    Console.WriteLine("You have reached your target for the day!");
+                    Console.WriteLine($"Earned money: {allPrice}lv.");
+                    break;
+                }
+                input = Console.ReadLine();
+            }
+
+            if (allPrice < goalOfTheDay)
+            {
+                double moneyNeeded = (double)goalOfTheDay - allPrice;
+                Console.WriteLine($"Target not reached! You need {moneyNeeded}lv. more.");
+                Console.WriteLine($"Earned money: {allPrice}lv.");
+            }
+        }
+    }
+}
---
+++ Exam/PinCode/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace PinCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNum = int.Parse(Console.ReadLine());
+            int secondNum = int.Parse(Console.ReadLine());
+            int thirdNum = int.Parse(Console.ReadLine());
+            int counter = 0;
+            int l = 0;
+
+            for (int i = 2; i <= firstNum; i += 2)
+            {
+                for (int j = 2; j <= secondNum; j++)
+                {
+                    for (int k = 2; k <= thirdNum; k += 2)
+                    {
+                        if (j % 4 == 0 || j % 6 == 0 || j % 8 == 0 || j % 9 == 0) 
+                        {
+                            continue;
+                        }
+                        if (j % 4 == 0 && j % 6 == 0 && j % 8 == 0 && j % 9 == 0)
+                        {
+                            continue;
+                        }
+                        if (j % 4 == 0 && j % 6 == 0)
+                        {
+                            continue;
+                        }
+                        if (j % 6 == 0 && j % 8 == 0)
+                        {
+                            continue;
+                        }
+                        if (j % 8 == 0 && j % 9 == 0)
+                        {
+                            continue;
+                        }
+                        Console.WriteLine($"{i} {j} {k}");
+                    }
+                }
+            }
+        }
+    }
+}
