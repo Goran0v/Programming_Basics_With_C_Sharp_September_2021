920a6cad953d9c59ec8dabf78eeb49344c3da5f0
Lyudmil Goranov <102062855+Goran0v@users.noreply.github.com>
2024-08-18 14:27:20 +0300

    Add last 3 exercises

---
+++ FirstExamPrep/TouristShop/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace TouristShop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double budget = double.Parse(Console.ReadLine());
+            string nameOfProduct = Console.ReadLine();
+            double priceOfProduct = 0;
+            int counterOfProducts = 0;
+            double allPrice = 0;
+
+            while (nameOfProduct != "Stop")
+            {
+                priceOfProduct = double.Parse(Console.ReadLine());
+                counterOfProducts++;
+                if (counterOfProducts % 3 == 0)
+                {
+                    priceOfProduct /= 2;
+                }
+                allPrice += priceOfProduct;
+                if (allPrice > budget)
+                {
+                    double moneyNeeded = allPrice - budget;
+                    Console.WriteLine("You don't have enough money!");
+                    Console.WriteLine($"You need {moneyNeeded:f2} leva!");
+                    break;
+                }
+                nameOfProduct = Console.ReadLine();
+            }
+
+            if (budget >= allPrice)
+            {
+                Console.WriteLine($"You bought {counterOfProducts} products for {allPrice:f2} leva.");
+            }
+        }
+    }
+}
