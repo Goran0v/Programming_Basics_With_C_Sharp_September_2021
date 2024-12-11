831b48b73e202d6f0c845a72172b6a3f1e5de43b
Lyudmil Goranov <102062855+Goran0v@users.noreply.github.com>
2024-12-11 21:44:57 +0200

    Added the exercises

---
+++ FirstStepsInCoding/HelloWorld/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // ctrl + F5 to start
+            Console.WriteLine("Hello SoftUni");
+        }
+    }
+}
---
+++ FirstStepsInCoding/PetShop/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace PetShop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberForDogs = int.Parse(Console.ReadLine());
+            int numberForCats = int.Parse(Console.ReadLine());
+            double sum = numberForDogs * 2.5 + numberForCats * 4;
+            Console.WriteLine($"{sum} lv.");
+        }
+    }
+}
