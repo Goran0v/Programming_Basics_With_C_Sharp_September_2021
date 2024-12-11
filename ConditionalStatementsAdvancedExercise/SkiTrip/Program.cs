6c412c9451017eeb943f536a42130fa0c16ac278
Lyudmil Goranov <102062855+Goran0v@users.noreply.github.com>
2024-12-11 22:23:43 +0200

    Added the exercises

---
+++ ConditionalStatementsAdvancedExercise/SkiTrip/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace SkiTrip
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const double PRICE_FOR_ROOM_FOR_ONE_PERSON = 18;
+            const double PRICE_FOR_APARTMENT = 25;
+            const double PRICE_FOR_PRESIDENT_APARTMENT = 35;
+
+            int days = int.Parse(Console.ReadLine());
+            string typeOfRoom = Console.ReadLine();
+            string rating = Console.ReadLine();
+            int nights = days - 1;
+            double totalPrice = 0;
+
+            switch (typeOfRoom)
+            {
+                case "room for one person":
+                    totalPrice = nights * PRICE_FOR_ROOM_FOR_ONE_PERSON;
+                    break;
+                case "apartment":
+                    totalPrice = nights * PRICE_FOR_APARTMENT;
+                    if (days > 15)
+                    {
+                        totalPrice *= 0.5;
+                    }
+                    else if (days >= 10)
+                    {
+                        totalPrice *= 0.65;
+                    }
+                    else
+                    {
+                        totalPrice *= 0.6;
+                    }
+                    break;
+                case "president apartment":
+                    totalPrice = nights * PRICE_FOR_PRESIDENT_APARTMENT;
+                    if (days > 15)
+                    {
+                        totalPrice *= 0.8;
+                    }
+                    else if (days >= 10)
+                    {
+                        totalPrice *= 0.85;
+                    }
+                    else
+                    {
+                        totalPrice *= 0.9;
+                    }
+                    break;
+            }
+            switch (rating)
+            {
+                case "positive":
+                    totalPrice *= 1.25;
+                    break;
+                case "negative":
+                    totalPrice *= 0.9;
+                    break;
+            }
+
+            Console.WriteLine($"{totalPrice:f2}");
+        }
+    }
+}
---
+++ ConditionalStatementsAdvancedExercise/SummerOutfit/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace SummerOutfit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int degrees = int.Parse(Console.ReadLine());
+            string day = Console.ReadLine();
+
+            if (degrees >= 10 && degrees <= 18)
+            {
+                if (day == "Morning")
+                {
+                    Console.WriteLine($"It's {degrees} degrees, get your Sweatshirt and Sneakers.");
+                }
+                else if (day == "Afternoon")
+                {
+                    Console.WriteLine($"It's {degrees} degrees, get your Shirt and Moccasins.");
+                }
+                else if (day == "Evening")
+                {
+                    Console.WriteLine($"It's {degrees} degrees, get your Shirt and Moccasins.");
+                }
+            }
+
+            else if (degrees > 18 && degrees <= 24)
+            {
+                if (day == "Morning")
+                {
+                    Console.WriteLine($"It's {degrees} degrees, get your Shirt and Moccasins.");
+                }
+                else if (day == "Afternoon")
+                {
+                    Console.WriteLine($"It's {degrees} degrees, get your T-Shirt and Sandals.");
+                }
+                else if (day == "Evening")
+                {
+                    Console.WriteLine($"It's {degrees} degrees, get your Shirt and Moccasins.");
+                }
+            }
+
+            else if (degrees >= 25)
+            {
+                if (day == "Morning")
+                {
+                    Console.WriteLine($"It's {degrees} degrees, get your T-Shirt and Sandals.");
+                }
+                else if (day == "Afternoon")
+                {
+                    Console.WriteLine($"It's {degrees} degrees, get your Swim Suit and Barefoot.");
+                }
+                else if (day == "Evening")
+                {
+                    Console.WriteLine($"It's {degrees} degrees, get your Shirt and Moccasins.");
+                }
+            }
+        }
+    }
+}
