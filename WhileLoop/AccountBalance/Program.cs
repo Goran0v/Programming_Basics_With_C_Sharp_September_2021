27d02e27ca212f7a3383c13585e99d82bfe3b758
Lyudmil Goranov <102062855+Goran0v@users.noreply.github.com>
2024-12-11 22:31:07 +0200

    Added the exercises

---
+++ WhileLoop/AccountBalance/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace AccountBalance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            double sum = 0.0;
+
+            while (input != "NoMoreMoney")
+            {
+                double currentNum = double.Parse(input);
+                if (currentNum < 0)
+                {
+                    Console.WriteLine("Invalid operation!");
+                    //Console.WriteLine($"Total: {sum:f2}");
+                    break;
+                }
+                else if (currentNum > 0)
+                {
+                    Console.WriteLine($"Increase: {currentNum:f2}");
+                    sum += currentNum;
+                }
+                input = Console.ReadLine();
+            }
+            Console.WriteLine($"Total: {sum:f2}");
+        }
+    }
+}
---
+++ WhileLoop/ReadText/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace WhileCycle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            while (input != "Stop")
+            {
+                Console.WriteLine(input);
+                input = Console.ReadLine();
+            }
+        }
+    }
+}
