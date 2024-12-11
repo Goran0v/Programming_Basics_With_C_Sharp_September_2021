244a895a006cc9dfbc82378bebc13fb0ae35caec
Lyudmil Goranov <102062855+Goran0v@users.noreply.github.com>
2024-12-11 22:33:45 +0200

    Added the exercises

---
+++ WhileLoopExercise/OldBooks/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace WhileCycleExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string bookName = Console.ReadLine();
+            string currentBook = Console.ReadLine();
+            int br = 0;
+
+            while (currentBook != "No More Books")
+            {
+                if (currentBook == bookName)
+                {
+                    Console.WriteLine($"You checked {br} books and found it.");
+                    break;
+                }
+                
+                br++;
+                currentBook = Console.ReadLine();
+            }
+            if (currentBook == "No More Books")
+            {
+                Console.WriteLine("The book you search is not here!");
+                Console.WriteLine($"You checked {br} books.");
+            }
+            
+        }
+    }
+}
---
+++ WhileLoopExercise/Vacation/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Vacation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double moneyNeeded = double.Parse(Console.ReadLine());
+            double availableMoney = double.Parse(Console.ReadLine());
+
+            int days = 0;
+            int spendCount = 0;
+
+            while (spendCount != 5)
+            {
+                string input = Console.ReadLine();
+                double newMoney = double.Parse(Console.ReadLine());
+                days++;
+                if (input == "save")
+                {
+                    availableMoney += newMoney;
+                    spendCount = 0;
+                }
+                else if (input == "spend")
+                {
+                    spendCount++;
+                    if (newMoney > availableMoney)
+                    {
+                        availableMoney = 0;
+                    }
+                    else
+                    {
+                        availableMoney -= newMoney;
+                    }
+                }
+                if (availableMoney >= moneyNeeded)
+                {
+                    Console.WriteLine($"You saved the money for {days} days.");
+                    break;
+                }
+            }
+            if (spendCount == 5)
+            {
+                Console.WriteLine("You can't save the money.");
+                Console.WriteLine($"{days}");
+            }
+        }
+    }
+}
